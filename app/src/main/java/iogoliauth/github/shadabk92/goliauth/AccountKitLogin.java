c72e2281370fb3806e9eb806189512f9233877dc
shadabk <shadab.khatib@gmail.com>
2017-11-28 21:31:01 +0530

    Facebook Google and Github Login Activities Added

---
+++ app/src/main/java/iogoliauth/github/shadabk92/goliauth/AccountKitLogin.java
@@ -0,0 +1,13 @@
+package iogoliauth.github.shadabk92.goliauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountKitLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_kit_login);
+    }
+}
---
+++ app/src/main/java/iogoliauth/github/shadabk92/goliauth/GooglePlusLogin.java
@@ -0,0 +1,13 @@
+package iogoliauth.github.shadabk92.goliauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GooglePlusLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_plus_login);
+    }
+}
