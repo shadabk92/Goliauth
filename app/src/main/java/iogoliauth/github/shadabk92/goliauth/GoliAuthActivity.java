6e31380dec36240160f926b3c08fdf677d5f16f1
shadabk <shadab.khatib@gmail.com>
2017-11-28 19:25:55 +0530

    Goliauth Master Project

---
+++ app/src/main/java/iogoliauth/github/shadabk92/goliauth/GoliAuthActivity.java
@@ -0,0 +1,13 @@
+package iogoliauth.github.shadabk92.goliauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoliAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goli_auth);
+    }
+}
